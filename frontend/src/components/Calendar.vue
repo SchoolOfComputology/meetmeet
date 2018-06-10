32769d2d15ebca587340931e757a6ab8b74f420f
Carlos Olguin <juancarlos.olguin@yahoo.com>
2018-06-10 14:05:48 -0500

    Set up landing and login
    Proof of concept of how routes and Vue work.

---
+++ frontend/src/components/Calendar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="Calendar">
+  <div>Month</div>
+  Today is: {{ todayString }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calendar',
+  data () {
+    return {
+      todayString: new Date() + ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div {
+  background-color: gray;
+  color:black;
+}
+</style>
---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="Login">
+    <div class="loginBox">User Name: <input type="text" v-model="user.username"></div>
+    <div>Password: <input type="text" v-model="user.password"></div>
+    <div><button v-on:click="loginClick"> Login</button> </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      user: {
+        username: 'Place Holder',
+        password: 'hunter2'
+      }
+    }
+  },
+  methods: {
+    loginClick: function () {
+      alert('Wassup ' + this.user.username + '!!')
+      this.$router.push({ name: 'authenticated', query: { redirect: '/authenticated' } })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div {
+  background-color: white;
+  color:black;
+}
+</style>
